59291537f08ceffcb0600a370e107d38c7614a5f
Pierre Tembremande <pierre.tembremande@gmail.com>
2025-04-11 14:43:44 +0200

    Mise en place du profile dev

--- src/main/java/com/chronorest/tech/StoreConnexionInitializer.java
+++ src/main/java/com/chronorest/tech/StoreConnexionInitializer.java
@@ -6,11 +6,13 @@ import java.net.UnknownHostException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.web.client.RestClient;
 
 import jakarta.annotation.PostConstruct;
 
 @Configuration
+@Profile("dev")
 public class StoreConnexionInitializer {
 
     @Autowired
