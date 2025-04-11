2fa3348573fbd854d2dc28bef63473515408c15c
Guillaume Kesteman <guillaume.kesteman@enigma-school.com>
2025-04-11 11:10:07 +0200

    fix: change prices field to final in Prices class for immutability

--- src/main/java/com/chronorest/core/Prices.java
+++ src/main/java/com/chronorest/core/Prices.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 public class Prices {
     @NotEmpty(message = "Prices list cannot be empty")
-    private List<ProductPrice> prices;
+    private final List<ProductPrice> prices;
 
     public Prices(List<ProductPrice> prices) {
         this.prices = prices;
